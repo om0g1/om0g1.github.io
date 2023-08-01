a107853b76a5c922ec6271038aa976e4bc294709
Davis Odongo Omogi <102514766+om0g1@users.noreply.github.com>
2023-08-01 01:49:22 -0700

    v0.0.1

--- static/js/main.js
+++ static/js/main.js
@@ -1,133 +1,121 @@
-const hero = document.querySelector("#hero");
-const tiles = document.querySelectorAll(".fancy-tile, .wide-fancy-tile");
-const hackerMen = document.querySelectorAll("h1, h2, h4, p, .hacker-man");
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const blobs = document.querySelectorAll("#blob, #blob-top");
+const works = ["a Full stack Developer", "an Artist", "a Full Stack Web Developer", "a Front End Webdeveloper", "a Backend Webdeveloper", "a Musician", "a Photo editor"];
 
 const options = {
-    root: document.querySelector("body"),
-    rootMargin: "2000px 0px 2000px 0px",
-    threshold: 0,
+  root: document.querySelector("body"),
+  rootMargin: "2000px 0px 2000px 0px",
+  threshold: 0,
 };
 
-function hackerMan(event){
+function hackerMan(target) {
   let interval = null;
   let iteration = 0;
-  const target = event.target;
   const par_len = target.dataset.text.length;
   clearInterval(interval);
   interval = setInterval(() => {
-    target.innerText = target.innerText
+    target.innerText = target.dataset.text
       .split("")
       .map((letter, index) => {
-        if(index < iteration){return target.dataset.text[index];}
-        if (letter == " ") return " ";
-        return letters[Math.floor(Math.random() * 26)]
+        if (index < iteration) {
+          return target.dataset.text[index];
+        }
+        if (letter === " ") {
+          return " ";
+        }
+        return letters[Math.floor(Math.random() * 26)];
       })
-      .join(""); 
-    
-    if(iteration >= par_len){ 
+      .join("");
+
+    if (iteration >= par_len) {
       clearInterval(interval);
     }
-    
+
     iteration += par_len / 27;
   }, 30);
 }
 
 const hackerManObserver = new IntersectionObserver(entries => {
   entries.forEach(entry => {
-    if(entry.isIntersecting) hackerMan(entry);
-  })
-  }
-  , options
-);
+    if (entry.isIntersecting) hackerMan(entry.target);
+  });
+}, options);
 
-hackerMen.forEach(element => {
+document.querySelectorAll(".hacker-observe").forEach(element => {
   element.dataset.text = element.innerText;
-  element.onmouseover = hackerMan;
-})
+  hackerManObserver.observe(element);
+});
 
-var work_index = 0;
-const works = ["a Full stack Developer","an Artist","a Full Stack Web Developer", "a Front End Webdeveloper","a Backend Webdeveloper",
-              "a Musician","a Photo editor"];
-
-document.querySelectorAll(".hacker-observe").forEach(element => hackerManObserver.observe(element));
 const hackerNext = document.querySelector(".hacker-next");
+let work_index = 0;
 
-if (hackerNext != null) {
-setInterval(function (){
-  let interval = null;
-  let iteration = 0;
-  clearInterval(interval);
-  hackerNext.dataset.text = works[work_index];
-  interval = setInterval(() => {
-    hackerNext.innerText = hackerNext.dataset.text
-      .split("")
-      .map((letter, index) => {
-        if(index < iteration) {
-          return hackerNext.dataset.text[index];
-        } 
-        if (letter == " ") return " ";
-        return letters[Math.floor(Math.random() * 26)]
-      })
-      .join("");
-    
-    if(iteration >= hackerNext.dataset.text.length){ 
-      clearInterval(interval);
-    }
-    iteration += hackerNext.dataset.text.length / 27;
-  }, 30);
-  work_index += 1;
-  if (work_index >= works.length) work_index = 0;
-}, 5000);
+if (hackerNext !== null) {
+  setInterval(() => {
+    let interval = null;
+    let iteration = 0;
+    clearInterval(interval);
+    hackerNext.dataset.text = works[work_index];
+    interval = setInterval(() => {
+      hackerNext.innerText = hackerNext.dataset.text
+        .split("")
+        .map((letter, index) => {
+          if (index < iteration) {
+            return hackerNext.dataset.text[index];
+          }
+          if (letter === " ") {
+            return " ";
+          }
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+
+      if (iteration >= hackerNext.dataset.text.length) {
+        clearInterval(interval);
+      }
+      iteration += hackerNext.dataset.text.length / 27;
+    }, 30);
+    work_index = (work_index + 1) % works.length;
+  }, 5000);
+}
+
+document.body.onpointermove = event => {
+  const { clientX, clientY } = event;
+  blobs.forEach(blob => {
+    blob.style.left = `${clientX}px`;
+    blob.style.top = `${clientY}px`;
+  });
 };
-
-document.body.onpointermove = event =>{
-  const {clientX, clientY} = event;
-  blobs.forEach(blob => blob.animate({
-  left: `${clientX}px`,
-  top: `${clientY}px`,
-  }, {duration: 2500, fill: "forwards"})
-  )
-};
-
 
 const swiperOptions = {
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 3,
-    spaceBetween: 10,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    768: {
+      loop: true,
+      slidesPerView: 5,
+      centeredSlides: false,
     },
-    // navigation: {
-    //     nextEl: '.swiper-button-next',
-    //     prevEl: '.swiper-button-prev',
-    // },
-    breakpoints: {
-        768 : {
-            loop: true,
-            slidesPerView: 5,
-            centeredSlides: false,
-        },
-        992 : {
-            slidesPerView: 7,
-            centeredSlides: false,
-        }
+    992: {
+      slidesPerView: 7,
+      centeredSlides: false,
     }
+  }
 };
 
-if (new Swiper != null){
 const sswiper = new Swiper('.swiper', swiperOptions);
 const vaswiper = new Swiper("#vas", swiperOptions);
 const mswiper = new Swiper("#ms", swiperOptions);
 const cswiper = new Swiper("#cs", swiperOptions);
-}
 
 window.onload = function () {
   setTimeout(() => {
     document.getElementById("loader").style.display = "none";
-    document.querySelectorAll("h1, h2, .hacker-observe").forEach(entry => {hackerMan({"target": entry})});
+    document.querySelectorAll("h1, h2, .hacker-observe").forEach(hackerMan);
   }, 2000);
 };
